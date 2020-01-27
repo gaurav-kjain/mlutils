3ad4fdd090230f95f4d12f73a6024f91d8c40145
Gaurav Kumar Jain <gaurav.kjain@gmail.com>
2020-01-27 21:41:24 +0530

    New changes

--- nlp/nlp_utils.py
+++ nlp/nlp_utils.py
@@ -1,3 +1,9 @@
+"""
+This file implement NLP related utils
+Author: Gaurav Kumar Jain
+
+"""
+
 import matplotlib.pyplot as plt
 %matplotlib inline
 import seaborn as sns
@@ -24,13 +30,17 @@ def get_regexp_frequencies(regexp="", text=None):
     
     
 def generate_text_numerical_features(df, text_col, target, verbose=True):
+    text_col_df=df[text_col]
     #create total characters features in text column
-    df['total_chars_count']    = df[text_col].apply(len)
+    df['total_chars_count']        = text_col_df.apply(len)
     
     #create total numer of words features in text column 
-    df['num_words_count']      = df[text_col].apply(lambda x: len(x.split()))
+    df['num_words_count']          = text_col_df.apply(lambda x: len(x.split()))
     
     #create total numer of words features in text column 
-    df['capital_chars_count']  = df[text_col].apply(lambda x: sum(1 for c in x if c.isupper()))
+    df['capital_chars_count']      = text_col_df.apply(lambda x: sum(1 for c in x if c.isupper()))
+    
+    #create total numer of exclmation marks in text column 
+    df['exclamation_marks_count']  = text_col_df.apply(lambda x: x.count('!'))
     
     return df
